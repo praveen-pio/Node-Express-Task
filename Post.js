738e19c339f7dcd38fd5628f6618bde6d5dd80b9
praveen-pio <praveen@programmers.io>
2022-06-27 18:38:35 +0530

    1 file modified

--- Post.js
+++ Post.js
@@ -1,1 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+var users = [{
+id:101,
+name:'car',
+cost:500000,
+description:'new car'
+
+
+}];
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('api/users',function(req,res)
+{
+  return res.json(users);
+});
+
+app.post('api/users',function(req,res)
+{
+  var user = req.body.user;
+  users.push(user);
+  return res.send('Product added');
+});
+
+app.listen('3000',function(){
+console.log('server listening to port 3000')
+
+});
